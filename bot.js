fbd8a468064835be19ec21f9e35853385df54dc2
muhammedafsarms <muhammedafsarms@gmail.com>
2026-05-09 03:51:31 +0530

    updated bot

--- bot.js
+++ bot.js
@@ -1,3 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Bot is online!')
+})
+
+app.listen(3000, () => {
+  console.log('Web server running')
+})
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
